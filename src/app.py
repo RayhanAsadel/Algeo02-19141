87a48e4621da994fa29fef4e28db34a0dd8277aa
RayhanAsadel <64323504+RayhanAsadel@users.noreply.github.com>
2020-11-14 21:47:31 +0700

    Update App.py untuk tampilan web
    penambahan halaman About

--- src/app.py
+++ src/app.py
@@ -11,17 +11,22 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/')
 def index():
-    
     return render_template('search2.html')
     
 @app.route('/', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
-       f = request.files['dokumentxt']
+       f = request.files['file[]']
        filename = secure_filename(f.filename)
        
        f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
        return render_template('search2uploadsucces.html')
+
+@app.route('/about')
+def about_files():
+    return render_template('about.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
